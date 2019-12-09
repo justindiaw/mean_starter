1ed735ab438c889b1960ba8e0a75674a0e2c2cd2
justindiaw <110058259@qq.com>
2019-12-08 19:26:15 -0500

    cleaner coding

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,21 +1,47 @@
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import * as express from 'express';
+import { MongoClient } from 'mongodb';
 
-class App {
+import { CONNECTION_URL, DATABASE_NAME } from '../dbconfig';
+
+export default class App {
 
     public app: express.Application;
 
     constructor() {
         this.app = express();
-        this.config();
+        this.initDatabase(this.config.bind(this));
     }
 
-    private config(): void {
-        // support application/json type post data
+    start(port: number): void {
+        this.app.listen(port, () => {
+            console.log(`Express server listening on port ${port}...`);
+        });
+    }
+
+    private config(db: any): void {
         this.app.use(bodyParser.json());
-        // support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(cors());
+        this.app.get('/test', (request, response) => {
+            db.collection('teachers')
+                .find({})
+                .toArray((error, result) => {
+                    response.send(result);
+                });
+        });
+    }
+
+
+    private initDatabase(config: Function): void {
+        MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+            if (error) {
+                throw error;
+            }
+            const db = client.db(DATABASE_NAME);
+            console.log(`Connected to database: ${DATABASE_NAME}!`);
+            config(db);
+        });
     }
 }
-
-export default  new App().app;
--- server/dbconfig.ts
+++ server/dbconfig.ts
@@ -1,19 +1,3 @@
-import { MongoClient } from 'mongodb';
 
-const CONNECTION_URL = 'mongodb+srv://developer:Ss110110@cluster0-yugia.mongodb.net/test?retryWrites=true&w=majority';
-const DATABASE_NAME = 'manage';
-let db: any; let collection;
-
-export function getConnection() {
-    return MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if (error) {
-            throw error;
-        }
-        db = client.db(DATABASE_NAME);
-        collection = db.collection('teachers');
-        collection.findOne({}, (err, result) => console.log(result));
-        console.log(`Connected to ${DATABASE_NAME}!`);
-    });
-}
-
-export default db;
+    export const CONNECTION_URL = 'mongodb+srv://developer:Ss110110@cluster0-yugia.mongodb.net/test?retryWrites=true&w=majority';
+    export const DATABASE_NAME = 'manage';
